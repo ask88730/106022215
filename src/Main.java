e0ed9cc391a2c8c44e620e9d520fcff3cf77854c
ask88730 <ask88730000@gmail.com>
2017-09-14 14:42:33 +0800

    班級姓名

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args)
     {
-	System.out.print("my first     java code!!");
+	System.out.print("學號:106022215\n系級:商應1A\n姓名:張志遠\n時間:2017/09/14\n教室:\"i329\""
+    );
     }
 }
